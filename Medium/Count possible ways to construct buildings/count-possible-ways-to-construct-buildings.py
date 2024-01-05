cdfd08f6c98a31d65d0cb8e26da4f2cd0e7ba171
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2024-01-05 22:27:01 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count possible ways to construct buildings/count-possible-ways-to-construct-buildings.py
@@ -0,0 +1,24 @@
+class Solution:
+    def TotalWays(self, N):
+        b= s = 1
+        res = 0
+        mod = 1000000007
+        for i in range(N):
+            res = (b%mod+s%mod)%mod
+            b = s%mod
+            s = res%mod
+        return (res*res)%mod
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		N = int(input())
+		ob = Solution()
+		ans = ob.TotalWays(N)
+		print(ans)
+# } Driver Code Ends
