dbbf84ac614c545aa6d66058006917d6a28f6e3d
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2024-05-29 22:39:15 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Geek and its Game of Coins/geek-and-its-game-of-coins.py
@@ -0,0 +1,34 @@
+
+class Solution:
+    def findWinner(self, n,x,y):
+        dp=[0]*(n+1)
+        for i in range(n):
+            if dp[i]==0:
+                dp[i+1]=1
+                if i+x<=n:
+                    dp[i+x]=1
+                if i+y<=n:
+                    dp[i+y]=1
+        return dp[n]
+        
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        n = int(input())
+
+        x = int(input())
+
+        y = int(input())
+
+        obj = Solution()
+        res = obj.findWinner(n, x, y)
+
+        print(res)
+
+# } Driver Code Ends
