bd34dd4fd67a446f25f6d162b812e832061687a0
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2024-06-25 06:56:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Left Rotate Matrix K times/left-rotate-matrix-k-times.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rotateMatrix(self, k, mat):
+        k = k%len(mat[0])
+        return [i[k:]+i[:k] for i in mat]
+
+
+
+#{ 
+ # Driver Code Starts
+import math
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n, m, k = map(int, input().strip().split(" "))
+        mat = []
+        for i in range(n):
+            mat.append(list(map(int, input().strip().split(" "))))
+        ob = Solution()
+        ans = ob.rotateMatrix(k, mat)
+        for i in range(n):
+            for j in range(m):
+                print(ans[i][j], end=" ")
+            print()
+
+# } Driver Code Ends
