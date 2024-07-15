47cd8c47d615e34a15cd674f32ccb3f73171228a
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2024-07-15 19:51:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest number/smallest-number.py
@@ -0,0 +1,41 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+class Solution:
+    def smallestNumber(self, s, d):
+        for i in range (pow(10,d-1),pow(10,d)+1):
+            k=i
+            sum=0
+            while (k>0):
+                r=k%10
+                sum+=r
+                k=k//10
+            if sum==s:
+                return i
+        return -1
+        
+
+
+#{ 
+ # Driver Code Starts.
+# Position this line where user code will be pasted.
+
+import sys
+import math
+input = sys.stdin.read
+data = input().split()
+
+t = int(data[0])
+index = 1
+
+for _ in range(t):
+    s = int(data[index])
+    d = int(data[index + 1])
+    index += 2
+    ob = Solution()
+    print(ob.smallestNumber(s, d))
+
+# } Driver Code Ends
