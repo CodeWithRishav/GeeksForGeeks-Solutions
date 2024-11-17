9841093df0237fddd9f7212ee69e3cb0290f6480
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2024-11-17 21:17:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.py
@@ -0,0 +1,49 @@
+class Solution:
+    def reverseArray(self, arr):
+        # code here
+        arrRev = []
+        for i in range(1,len(arr)+1):
+            arrRev.append(arr[-i])
+        
+        for i in range(len(arrRev)):
+            arr[i] = arrRev[i]
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+import sys
+
+
+def main():
+    # Read the number of test cases
+    tc = int(sys.stdin.readline())
+
+    while tc > 0:
+        # Read the array elements as a string
+        str_arr = sys.stdin.readline().split()
+
+        # Convert the string array to an integer array
+        arr = [int(x) for x in str_arr]
+
+        # Create a Solution object
+        obj = Solution()
+
+        # Reverse the array
+        obj.reverseArray(arr)
+
+        # Print the reversed array
+        for i in range(0, len(arr)):
+            print(arr[i], end=" ")
+        print()
+        print("~")
+
+        # Decrement the test case count
+        tc -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
