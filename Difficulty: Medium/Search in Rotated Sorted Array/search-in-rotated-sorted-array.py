84f4c2e509d4817116df885a5a7e15c388d628f1
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2024-12-14 23:12:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.py
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.py
@@ -1,12 +1,46 @@
 #User function Template for python3
 
 class Solution:
-    def search(self,arr,key):
-        for i in range(0,len(arr)):
-            if arr[i] == key:
-                return i
-        return -1
 
+    def search(self, arr, key):
+        # Initialize two pointers, lo and hi, at the start
+        # and end of the array
+        lo = 0
+        hi = len(arr) - 1
+
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+
+            # If key found, return the index
+            if arr[mid] == key:
+                return mid
+
+            # If Left half is sorted
+            if arr[mid] >= arr[lo]:
+
+                # If the key lies within this sorted half,
+                # move the hi pointer to mid - 1
+                if key >= arr[lo] and key < arr[mid]:
+                    hi = mid - 1
+
+                # Otherwise, move the lo pointer to mid + 1
+                else:
+                    lo = mid + 1
+
+            # If Right half is sorted
+            else:
+
+                # If the key lies within this sorted half,
+                # move the lo pointer to mid + 1
+                if key > arr[mid] and key <= arr[hi]:
+                    lo = mid + 1
+
+                # Otherwise, move the hi pointer to mid - 1
+                else:
+                    hi = mid - 1
+
+        # Key not found
+        return -1
 
 #{ 
  # Driver Code Starts
@@ -20,5 +54,6 @@ if __name__ == '__main__':
         k = int(input())
         ob = Solution()
         print(ob.search(A, k))
+        print("~")
 
 # } Driver Code Ends
