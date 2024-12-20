8994bbe332f0fc7965fbe3191ef07e146bbe034c
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2024-12-20 21:11:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.py
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.py
@@ -1,37 +1,38 @@
 class Solution:
-    def spirallyTraverse(self, matrix):
-        if not matrix or not matrix[0]:
-            return []
+    # Function to return a list of integers denoting spiral traversal of matrix.
+    def spirallyTraverse(self, mat):
+        r = len(mat)
+        c = len(mat[0])
+        output = []
 
-        result = []
-        top, bottom = 0, len(matrix) - 1
-        left, right = 0, len(matrix[0]) - 1
+        row = 0
+        col = 0
 
-        while top <= bottom and left <= right:
-            # Traverse from left to right along the top row
-            for i in range(left, right + 1):
-                result.append(matrix[top][i])
-            top += 1
-            
-            # Traverse from top to bottom along the right column
-            for i in range(top, bottom + 1):
-                result.append(matrix[i][right])
-            right -= 1
-            
-            if top <= bottom:
-                # Traverse from right to left along the bottom row
-                for i in range(right, left - 1, -1):
-                    result.append(matrix[bottom][i])
-                bottom -= 1
-            
-            if left <= right:
-                # Traverse from bottom to top along the left column
-                for i in range(bottom, top - 1, -1):
-                    result.append(matrix[i][left])
-                left += 1
-        
-        return result
+        while row < r and col < c:
+            # storing the elements of 1st row from the remaining rows, in a list.
+            for i in range(col, c):
+                output.append(mat[row][i])
+            row += 1
 
+            # storing elements of last column from remaining columns, in list.
+            for i in range(row, r):
+                output.append(mat[i][c - 1])
+            c -= 1
+
+            # storing the elements of last row from remaining rows, in a list.
+            if row < r:
+                for i in range(c - 1, col - 1, -1):
+                    output.append(mat[r - 1][i])
+                r -= 1
+
+            # storing elements of 1st column from the remaining columns, in list.
+            if col < c:
+                for i in range(r - 1, row - 1, -1):
+                    output.append(mat[i][col])
+                col += 1
+
+        # returning the list.
+        return output
 
 #{ 
  # Driver Code Starts
@@ -55,5 +56,6 @@ if __name__ == "__main__":
         solution = Solution()
         result = solution.spirallyTraverse(matrix)
         print(" ".join(map(str, result)))
+        print("~")
 
 # } Driver Code Ends
