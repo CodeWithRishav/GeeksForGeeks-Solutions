6342ced35675e732db30943e7ef5561969f133ec
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2025-02-21 16:30:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.py
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.py
@@ -1,20 +1,31 @@
-#User function Template for python3
 
 class Solution:
-    def ispar(self, x):
-        stack = []
-        matching_bracket = {')': '(', '}': '{', ']': '['}
-        
-        for char in x:
-            if char in matching_bracket.values():
-                stack.append(char)
-            elif char in matching_bracket:
-                if not stack or stack[-1] != matching_bracket[char]:
-                    return False
-                stack.pop()
-        
-        return not stack
 
+    def isBalanced(self, s):
+
+        # Declare a stack to store the opening brackets
+        st = []
+        for i in range(len(s)):
+
+            # Check if the character is an opening bracket
+            if s[i] == '(' or s[i] == '{' or s[i] == '[':
+                st.append(s[i])
+
+            else:
+                # If it's a closing bracket, check if the stack is non-empty
+                # and if the top of the stack is a matching opening bracket
+                if st and ((st[-1] == '(' and s[i] == ')') or
+                           (st[-1] == '{' and s[i] == '}') or
+                           (st[-1] == '[' and s[i] == ']')):
+
+                    # Pop the matching opening bracket
+                    st.pop()
+                else:
+                    # Unmatched closing bracket
+                    return False
+
+        # If stack is empty, return True (balanced), otherwise False
+        return not st
 
 #{ 
  # Driver Code Starts
@@ -40,14 +51,12 @@ def write():
 if __name__ == '__main__':
     test_cases = int(input())
     for cases in range(test_cases):
-        #n = int(input())
-        #n,k = map(int,imput().strip().split())
-        #a = list(map(int,input().strip().split()))
         s = str(input())
         obj = Solution()
-        if obj.ispar(s):
-            print("balanced")
+        if obj.isBalanced(s):
+            print("true")
         else:
-            print("not balanced")
+            print("false")
+        print("~")
 
 # } Driver Code Ends
