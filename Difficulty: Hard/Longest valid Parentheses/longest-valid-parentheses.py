25425013b70283e76138548f4a580c46dd59c385
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2025-02-22 23:03:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.py
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.py
@@ -1,20 +1,38 @@
-# User function Template for Python3
 
 class Solution:
-    def maxLength(self, str):
-        stack = [-1]
-        count = 0
-        for i, char in enumerate(str):
-            if char == '(':
+
+    def maxLength(self, s):
+        stack = []
+
+        # Push -1 as the initial index to
+        # handle the edge case
+        stack.append(-1)
+        maxLen = 0
+
+        # Traverse the string
+        for i in range(len(s)):
+
+            # If we encounter an opening parenthesis,
+            # push its index
+            if s[i] == '(':
                 stack.append(i)
             else:
+
+                # If we encounter a closing parenthesis,
+                # pop the stack
                 stack.pop()
+
+                # If stack is empty, push the current index
+                # as a base for the next valid substring
                 if not stack:
                     stack.append(i)
                 else:
-                    count = max(count, i - stack[-1])
-        
-        return count
+
+                    # Update maxLength with the current length
+                    # of the valid parentheses substring
+                    maxLen = max(maxLen, i - stack[-1])
+
+        return maxLen
 
 
 #{ 
@@ -28,5 +46,6 @@ if __name__ == '__main__':
 
         ob = Solution()
         print(ob.maxLength(S))
+        print("~")
 
 # } Driver Code Ends
