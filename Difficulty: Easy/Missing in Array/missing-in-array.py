b1ec4d5b0a4e72deceff31f63a0f71e3735279aa
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2025-04-21 08:57:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.py
+++ Difficulty: Easy/Missing in Array/missing-in-array.py
@@ -1,13 +1,20 @@
-#User function Template for python3
 class Solution:
-    
-    # Note that the size of the array is n-1
-    def missingNumber(self, n, arr):
-        
-        # code here
-        tot=n*(n+1)/2
-        ar_sum=sum(arr)
-        return int(tot-ar_sum)
+    # Function to find the missing number in the array
+    def missingNum(self, arr):
+        n = len(arr) + 1
+        xor1 = 0
+        xor2 = 0
+
+        # XOR all array elements
+        for num in arr:
+            xor2 ^= num
+
+        # XOR all numbers from 1 to n
+        for i in range(1, n + 1):
+            xor1 ^= i
+
+        # Missing number is the XOR of xor1 and xor2
+        return xor1 ^ xor2
 
 
 #{ 
@@ -16,9 +23,9 @@ class Solution:
 
 t = int(input())
 for _ in range(0, t):
-    n = int(input())
     arr = list(map(int, input().split()))
-    s = Solution().missingNumber(n, arr)
+    s = Solution().missingNum(arr)
     print(s)
 
+    print("~")
 # } Driver Code Ends
