6d94612c9ac726ef030d0dcb5bfea11e2cacc692
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2025-05-06 18:37:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.py
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.py
@@ -11,101 +11,120 @@ class Node:
 '''
 
 #Function to return a list containing elements of left view of the binary tree.
-def LeftView(root):
-    # code here
-    que=deque([root])
-    res=[]
-    while que:
-        n,i=len(que),0
-        for _ in range(n):
-            cur=que.popleft()
-            if i==0:
-                res.append(cur.data)
-            if cur.left:
-                que.append(cur.left)
-            if cur.right:
-                que.append(cur.right)
-            i+=1
-    return res
+class Solution:
 
+    def leftViewUtil(self, result, root, level):
+        global max_level
+
+        #if root is null, we simply return.
+        if (root == None):
+            return
+
+        #if this is the first root of its level then it is in the left view.
+        if (max_level < level):
+            #storing data of current root in list.
+            result.append(root.data)
+            max_level = level
+
+        #calling function recursively for left and right subtrees of current root.
+        self.leftViewUtil(result, root.left, level + 1)
+        self.leftViewUtil(result, root.right, level + 1)
+
+    #Function to return a list containing elements of left view of the binary tree.
+    def LeftView(self, root):
+        global max_level
+        max_level = 0
+        result = []
+        self.leftViewUtil(result, root, 1)
+        #returning the list.
+        return result
 
 #{ 
  # Driver Code Starts
 #Contributed by Sudarshan Sharma
 from collections import deque
+
+
 # Tree Node
 class Node:
+
     def __init__(self, val):
         self.right = None
         self.data = val
         self.left = None
 
-# Function to Build Tree   
+
+# Function to Build Tree
 def buildTree(s):
     #Corner Case
-    if(len(s)==0 or s[0]=="N"):           
+    if (len(s) == 0 or s[0] == "N"):
         return None
-        
-    # Creating list of strings from input 
+
+    # Creating list of strings from input
     # string after spliting by space
-    ip=list(map(str,s.split()))
-    
+    ip = list(map(str, s.split()))
+
     # Create the root of the tree
-    root=Node(int(ip[0]))                     
-    size=0
-    q=deque()
-    
+    root = Node(int(ip[0]))
+    size = 0
+    q = deque()
+
     # Push the root to the queue
-    q.append(root)                            
-    size=size+1 
-    
+    q.append(root)
+    size = size + 1
+
     # Starting from the second element
-    i=1                                       
-    while(size>0 and i<len(ip)):
+    i = 1
+    while (size > 0 and i < len(ip)):
         # Get and remove the front of the queue
-        currNode=q[0]
+        currNode = q[0]
         q.popleft()
-        size=size-1
-        
+        size = size - 1
+
         # Get the current node's value from the string
-        currVal=ip[i]
-        
+        currVal = ip[i]
+
         # If the left child is not null
-        if(currVal!="N"):
-            
+        if (currVal != "N"):
+
             # Create the left child for the current node
-            currNode.left=Node(int(currVal))
-            
+            currNode.left = Node(int(currVal))
+
             # Push it to the queue
             q.append(currNode.left)
-            size=size+1
+            size = size + 1
         # For the right child
-        i=i+1
-        if(i>=len(ip)):
+        i = i + 1
+        if (i >= len(ip)):
             break
-        currVal=ip[i]
-        
+        currVal = ip[i]
+
         # If the right child is not null
-        if(currVal!="N"):
-            
+        if (currVal != "N"):
+
             # Create the right child for the current node
-            currNode.right=Node(int(currVal))
-            
+            currNode.right = Node(int(currVal))
+
             # Push it to the queue
             q.append(currNode.right)
-            size=size+1
-        i=i+1
+            size = size + 1
+        i = i + 1
     return root
-    
-    
-if __name__=="__main__":
-    t=int(input())
-    for _ in range(0,t):
-        s=input()
-        root=buildTree(s)
-        result = LeftView(root)
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(0, t):
+        s = input()
+        root = buildTree(s)
+        ob = Solution()
+        result = ob.LeftView(root)
+        if len(result) == 0:
+            print("[]", end=" ")
         for value in result:
-            print(value,end=" ")
+            print(value, end=" ")
         print()
+
+        print("~")
 
 # } Driver Code Ends
