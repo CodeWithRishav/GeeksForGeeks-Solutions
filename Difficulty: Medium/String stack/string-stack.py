e819398e04d48c518abbec43ef644f6bd0b154d6
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2025-09-15 08:51:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/String stack/string-stack.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def stringStack(self, pat, tar):
+        i = len(pat) - 1
+        j = len(tar) - 1
+
+        while i >= 0 and j >= 0:
+
+            # If characters don’t match, simulate a deletion
+            # by skipping previous character in 'pat'
+            if pat[i] != tar[j]:
+                i -= 2
+
+            # If characters match, move both
+            # pointers to the previous character
+            else:
+                i -= 1
+                j -= 1
+
+        # If we have successfully matched all
+        # characters of 'tar', return true
+        return j < 0
